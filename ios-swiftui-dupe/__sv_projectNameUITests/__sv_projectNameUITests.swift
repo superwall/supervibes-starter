d79c614e19411fb9df665e2393c98da680ecc2f8
jakemor <jakemny@gmail.com>
2025-09-12 16:20:49 -0400

    pushes a test duplicate project

---
+++ ios-swiftui-dupe/__sv_projectNameUITests/__sv_projectNameUITests.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+final class __sv_projectNameUITests: XCTestCase {
+  override func setUpWithError() throws {
+    continueAfterFailure = false
+  }
+
+  func testLaunch() throws {
+    let app = XCUIApplication()
+    app.launch()
+  }
+}
